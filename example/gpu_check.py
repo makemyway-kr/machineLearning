c8683cc3effc43b05921e0d563f814672ce6b002
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-04-16 16:16:08 +0900

    gpu

--- example/gpu_check.py
+++ example/gpu_check.py
@@ -1,2 +1,4 @@
+import os
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
 from tensorflow.python.client import device_lib 
 print(device_lib.list_local_devices())
