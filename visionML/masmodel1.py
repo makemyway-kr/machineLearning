cbfb04b8d241d955f6b5eec626ac0f975aa1a258
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-05-24 12:37:21 +0900

    model1

--- visionML/masmodel1.py
+++ visionML/masmodel1.py
@@ -8,67 +8,83 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten, BatchNormalization
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+baseDir = '/mnt/hdd/vision_data/'
+with tf.device("/GPU:0"):
+    images = glob(baseDir+'data1/images/*.jpg')
+    faces = []
+    face_result_data = []
+    face_whole = []
+    #dataset 얼굴범위 인식 결과 로드
+    with open('./result.json','r',encoding='utf-8') as f:
+        face_whole = json.load(f)
+    face_result_data = list(face_whole.keys()) 
+    #이미지 리사이징
+    image_array = np.float32(np.zeros((2000,224,224,3)))#램 제한으로 인한 데이터 수 줄임
+    labels = np.float64(np.zeros((2000,1)))
+    count = 0
+    for k in face_result_data: #라벨과 이미지 담기
+        if count == 2000:
+            break
+        print(k)
+        try:
+            #face = cv2.imread(k)
+            coordinate = face_whole[k]['face']
+            if len(coordinate) != 0 :
+                #cv2.imwrite(baseDir+'onlyMask/'+str(k.split('/')[6]),face[coordinate[1]:coordinate[3],coordinate[0]: coordinate[2] , :])
+                img = load_img(baseDir + 'onlyMask/'+str(k.split('/')[6]),target_size = (224 , 224))
+                i = img_to_array(img)
+                i = np.expand_dims(i,axis = 0)
+                i = preprocess_input(i)
+                image_array[count,:,:,:] = i
+                labels[count] = int(k.split('_')[2][0]) #이미지 이름에 라벨이 있음.
+            else:
+                continue
+        except Exception as ex:
+            print(ex)
+            continue
+        count += 1
 
-images = glob('/content/drive/MyDrive/part_of_data/*.jpg')
-faces = []
-face_result_data = []
-#dataset 얼굴범위 인식 결과 로드
-with open('/content/drive/MyDrive/result.json','r',encoding='utf-8') as f:
-    face_result_data = json.load(f)
-#이미지 리사이징
-image_array = np.float32(np.zeros((len(face_result_data.keys()),224,224,3)))
-labels = np.float64(np.zeros((len(face_result_data),1)))
-count = 0
-for k in face_result_data.keys(): #라벨과 이미지 담기
-    face = cv2.imread(k)
-    coordinate = face_result_data[k]['face']
-    cv2.imwrite('/content/drive/MyDrive/onlyMask'+str(k.split('\\')[1]),face[coordinate[1]:coordinate[3],coordinate[0]: coordinate[2] , :])
-    img = load_img('/content/drive/MyDrive/onlyMask'+str(k.split('\\')[1]),target_size = (224 , 224))
+    #TRAIN / test data 나누기
+    trainImgCount = int(np.round(labels.shape[0]*0.8)) #80퍼센트 트레이닝 데이터로
+    testImgCount = int(np.round(labels.shape[0]*0.8))
 
-    i = img_to_array(img)
-    i = np.expand_dims(i,axis = 0)
-    i = preprocess_input(i)
-    image_array[count,:,:,:] = i
-    labels[count] = k.split('_')[1] #이미지 이름에 라벨이 있음.
+    trainImg = image_array[0:trainImgCount,:,:,:]
+    testImg = image_array[trainImgCount:,:,:,:]
 
-#TRAIN / test data 나누기
-trainImgCount = int(np.round(labels.shape[0]*0.8)) #80퍼센트 트레이닝 데이터로
-testImgCount = int(np.round(labels.shape[0]*0.8))
+    trainLabel = labels[0:trainImgCount]
+    testLabel = labels[trainImgCount:]
 
-trainImg = image_array[0:trainImgCount,:,:,:]
-testImg = image_array[trainImgCount:,:,:,:]
+    imgShape = (224,224,3)
 
-trainLabel = labels[0:trainImgCount]
-testLabel = labels[trainImgCount:]
+    resnetBase = ResNet50(input_shape = imgShape , weights = 'imagenet',include_top = False)
+    resnetBase.trainable = False
+    resnetBase.summary()
 
-imgShape = (224,224,3)
+    fL = Flatten()
+    dL1 = Dense(128,activation = 'relu')
+    bnL1 = BatchNormalization()
+    dL2 = Dense(64,activation = 'relu')
+    bnL2 = BatchNormalization()
+    dL3 = Dense(16,activation = 'relu')
+    bnL3 = BatchNormalization()
+    dL4 = Dense(4,activation = tf.keras.activations.softmax) #라벨과 같은 4개로 분류, softmax사용
 
-resnetBase = ResNet50(input_shape = imgShape , weights = 'imagenet',include_top = False)
-resnetBase.trainable = True
-resnetBase.summary()
+    model = Sequential([
+        resnetBase,
+        fL,
+        dL1,
+        bnL1,
+        dL2,
+        bnL2,
+        dL4,]
+    )
+    learningRate = 0.00001
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=learningRate), loss = tf.keras.losses.MeanSquaredError() , metrics = ['accuracy'])
+    model.summary()
 
-fL = Flatten()
-dL1 = Dense(128,activation = 'relu')
-bnL1 = BatchNormalization()
-dL2 = Dense(4,activation = tf.keras.activations.softmax) #라벨과 같은 4개로 분류, softmax사용
+    model.fit(trainImg,trainLabel,epochs = 10000 , batch_size  = 10 , validation_data = (testImg,testLabel))
 
-model = Sequential(
-    resnetBase,
-    fL,
-    dL1,
-    bnL1,
-    dL2,
-)
-learningRate = 0.001
-model.compile(optimizer=tf.keras.optimizers.Adam(lr=learningRate), loss = tf.keras.losses.MeanSquaredError() , metrics = ['accuracy'])
-model.summary()
-
-with tf.device("/gpu:0"):
-    model.fit(trainImg,trainLabel,epochs = 100 , batch_size  = 50 , validation_data = (testImg,testLabel))
-
-model.save("model.M1")
+    model.save("model.M1_2")
 
 
