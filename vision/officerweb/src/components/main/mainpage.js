09e9a77105810653fad61487120a77a2d7ab9fed
SamSim <jun78111209@gmail.com>
2022-05-31 18:14:24 +0900

    complete

--- vision/officerweb/src/components/main/mainpage.js
+++ vision/officerweb/src/components/main/mainpage.js
@@ -2,11 +2,12 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useState } from "react";
 import socketIoClient from 'socket.io-client';
+import { Buffer } from 'buffer';
 import './main.css';
 
 const socketServer = 'http://192.168.0.9:5555'; //서버 주소
 
-const socket = socketIoClient.connect(socketServer);
+const socket = socketIoClient.connect(socketServer,{ transports: ['websocket', 'polling', 'flashsocket'] });
 
 const PageBody = styled.div`
     width : 100%;
@@ -17,6 +18,7 @@ const PageBody = styled.div`
 const ImageBox = styled.div`
     width : 80%;
     height : 60%;
+    justify-content: center;
     object-fit : cover;    
 `
 
@@ -29,15 +31,17 @@ const MainPage = () => {
     }, []);
 
     useEffect(() => {
-        socket.on('processResult', (data) => {
-            setImage(Buffer.from(data, "base64").toString());
+        socket.on('result', (data) => {
+            console.log('image comming');
+            console.log(data);
+            setImage([data[0],Buffer.from(data[1], "base64").toString()]);
         })
     })
 
     return (<PageBody >
         <div className="title">마스크 미착용자 알림 프로그램</div>
         <div className="type">{image[0]}</div>
-        <ImageBox>{ image[1] !== '' &&<img src={"data:image/jpeg;base64," + image[1]} />}</ImageBox>
+        <ImageBox>{ image[1] !== '' &&<img class = "maskImg"src={"data:image/jpeg;base64," + image[1]} />}</ImageBox>
     </PageBody>
     )
 }
